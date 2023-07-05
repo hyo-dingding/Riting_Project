87bee0efc49f4a0f1bf24e3a7835b5c3ef6e7e4b
hyo-dingding <zvoniti2964@gmail.com>
2023-07-05 20:23:58 +0900

    chore: test 수정

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,26 +1,32 @@
-import { INestApplication } from "@nestjs/common";
+import { HttpStatus, INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "../src/app.module";
 import request from "supertest";
-import { JwtAccessStrategy } from "../src/commons/auth/jwt-access.strategy";
-import { JwtRefreshStrategy } from "../src/commons/auth/jwt-refresh.strategy";
 
-let accessToken: string;
-let email: string;
+let accessToken = null;
+let email = null;
+let req: request.SuperTest<request.Test>;
 
-describe("AppController (e2e)", () => {
+describe("회원 (e2e) ", () => {
     let app: INestApplication;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
-            providers: [JwtAccessStrategy, JwtRefreshStrategy],
         }).compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
 
-        const res = await request(app.getHttpServer()).post("/user/signup").send({
+        req = request(app.getHttpServer());
+
+        const existingUserRes = await req.post("/user/login").send({ email: "test@test.com", password: "1234" });
+
+        if (existingUserRes.statusCode !== HttpStatus.OK) {
+            await req.post("/user/signup").send({ email: "test@test.com", username: "test", password: "1234" });
+        }
+
+        const res = await req.post("/user/login").send({
             email: "test@test.com",
             password: "1234",
         });
@@ -39,7 +45,6 @@ describe("AppController (e2e)", () => {
                     username: "test1",
                     password: "1234",
                 });
-
                 expect(res.statusCode).toBe(201);
                 expect(res.body.data).toHaveProperty("accessToken");
                 return;
@@ -51,7 +56,6 @@ describe("AppController (e2e)", () => {
                     username: "test",
                     password: "1234",
                 });
-
                 expect(res.statusCode).toBe(400);
                 return;
             });
@@ -62,47 +66,45 @@ describe("AppController (e2e)", () => {
                     username: "test",
                     password: "1234",
                 });
-
                 expect(res.statusCode).toBe(409);
                 // expect(res.body.code).toBe('CONFLICT_EMAIL');
                 return;
             });
-        });
 
-        // 로그인 API 테스트
+            // 로그인 API 테스트
 
-        describe("로그인", () => {
-            it("로그인 성공 200 /user/login (POST)", async () => {
-                const res = await request(app.getHttpServer()).post("/user/login").send({
-                    email: "test@test.com",
-                    password: "1234",
+            describe("로그인", () => {
+                it("로그인 성공 200 /user/login (POST)", async () => {
+                    const res = await request(app.getHttpServer()).post("/user/login").send({
+                        email: "test@test.com",
+                        password: "1234",
+                    });
+
+                    expect(res.statusCode).toBe(200);
+                    expect(res.body.data).toHaveProperty("accessToken");
+                    return;
                 });
 
-                expect(res.statusCode).toBe(200);
-                expect(res.body.data).toHaveProperty("accessToken");
-                return;
-            });
+                it("로그인 실패 401 /user/signin (POST)", async () => {
+                    const res = await request(app.getHttpServer()).post("/user/login").send({
+                        email: "test",
+                        password: "1234",
+                    });
 
-            it("로그인 실패 401 /user/login (POST)", async () => {
-                const res = await request(app.getHttpServer()).post("/user/login").send({
-                    email: "test",
-                    password: "1234",
+                    expect(res.statusCode).toBe(401);
+                    return;
                 });
 
-                expect(res.statusCode).toBe(401);
-                expect(res.body.data).toBeUndefined();
-                return;
-            });
+                it("로그인 실패 401 /user/login (POST)", async () => {
+                    const res = await request(app.getHttpServer()).post("/user/login").send({
+                        email: "test@test.com",
+                        password: "12345",
+                    });
 
-            it("로그인 실패 401 /user/login (POST)", async () => {
-                const res = await request(app.getHttpServer()).post("/user/login").send({
-                    email: "test@test.com",
-                    password: "12345",
+                    expect(res.statusCode).toBe(401);
+                    // expect(res.body.code).toBe('UNAUTHORIZED_ACCOUNT');
+                    return;
                 });
-
-                expect(res.statusCode).toBe(401);
-                expect(res.body.data).toBeUndefined();
-                return;
             });
         });
     });
