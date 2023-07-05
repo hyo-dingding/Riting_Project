56a391481aef0740a0ff604822de6628718f1327
hyo-dingding <zvoniti2964@gmail.com>
2023-07-05 20:11:16 +0900

    chore: test 수정

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,100 +1,108 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import { beforeEach } from "node:test";
-import { AppModule } from "src/app.module";
+import { AppModule } from "../src/app.module";
 import request from "supertest";
+import { JwtAccessStrategy } from "../src/commons/auth/jwt-access.strategy";
+import { JwtRefreshStrategy } from "../src/commons/auth/jwt-refresh.strategy";
 
-// let accessToken = null;
-// let app: null;
+let accessToken: string;
+let email: string;
 
-describe("회원 (e2e) ", () => {
+describe("AppController (e2e)", () => {
     let app: INestApplication;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
+            providers: [JwtAccessStrategy, JwtRefreshStrategy],
         }).compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
+
         const res = await request(app.getHttpServer()).post("/user/signup").send({
             email: "test@test.com",
             password: "1234",
         });
-        accessToken = res.body.date.accessToken;
+
+        accessToken = res.body.data.accessToken;
     });
 
     // 회원 가입 API 테스트
 
-    let accessToken = null;
-    let email = null;
-
     describe("회원 API 테스트", () => {
         describe("회원가입", () => {
-            it("회원가입 성공 201 /users/signup (POST)", async () => {
+            it("회원가입 성공 201 /user/signup (POST)", async () => {
                 email = String(Math.floor(Math.random() * 10000000)) + "test@test.com";
-                const res = await request(app.getHttpServer()).post("/users/signup").send({
+                const res = await request(app.getHttpServer()).post("/user/signup").send({
                     email: email,
+                    username: "test1",
                     password: "1234",
                 });
+
                 expect(res.statusCode).toBe(201);
                 expect(res.body.data).toHaveProperty("accessToken");
                 return;
             });
 
-            it("회원가입 실패 400 /users/signup (POST)", async () => {
-                const res = await request(app.getHttpServer()).post("/users/signup").send({
+            it("회원가입 실패 400 /user/signup (POST)", async () => {
+                const res = await request(app.getHttpServer()).post("/user/signup").send({
                     email: "1234",
+                    username: "test",
                     password: "1234",
                 });
+
                 expect(res.statusCode).toBe(400);
                 return;
             });
 
-            it("회원가입 실패 409 /users/signup (POST)", async () => {
-                const res = await request(app.getHttpServer()).post("/users/signup").send({
+            it("회원가입 실패 409 /user/signup (POST)", async () => {
+                const res = await request(app.getHttpServer()).post("/user/signup").send({
                     email: email,
+                    username: "test",
                     password: "1234",
                 });
+
                 expect(res.statusCode).toBe(409);
-                expect(res.body.code).toBe("CONFLICT_EMAIL");
+                // expect(res.body.code).toBe('CONFLICT_EMAIL');
+                return;
+            });
+        });
+
+        // 로그인 API 테스트
+
+        describe("로그인", () => {
+            it("로그인 성공 200 /user/login (POST)", async () => {
+                const res = await request(app.getHttpServer()).post("/user/login").send({
+                    email: "test@test.com",
+                    password: "1234",
+                });
+
+                expect(res.statusCode).toBe(200);
+                expect(res.body.data).toHaveProperty("accessToken");
                 return;
             });
 
-            // 로그인 API 테스트
-
-            describe("로그인", () => {
-                it("로그인 성공 200 /users/signin (POST)", async () => {
-                    const res = await request(app.getHttpServer()).post("/users/signin").send({
-                        email: "test@test.com",
-                        password: "1234",
-                    });
-
-                    expect(res.statusCode).toBe(200);
-                    expect(res.body.data).toHaveProperty("accessToken");
-                    return;
+            it("로그인 실패 401 /user/login (POST)", async () => {
+                const res = await request(app.getHttpServer()).post("/user/login").send({
+                    email: "test",
+                    password: "1234",
                 });
 
-                it("로그인 실패 400 /users/signin (POST)", async () => {
-                    const res = await request(app.getHttpServer()).post("/users/signin").send({
-                        email: "test",
-                        password: "1234",
-                    });
+                expect(res.statusCode).toBe(401);
+                expect(res.body.data).toBeUndefined();
+                return;
+            });
 
-                    expect(res.statusCode).toBe(400);
-                    return;
+            it("로그인 실패 401 /user/login (POST)", async () => {
+                const res = await request(app.getHttpServer()).post("/user/login").send({
+                    email: "test@test.com",
+                    password: "12345",
                 });
 
-                it("로그인 실패 401 /users/signin (POST)", async () => {
-                    const res = await request(app.getHttpServer()).post("/users/signin").send({
-                        email: "test@test.com",
-                        password: "12345",
-                    });
-
-                    expect(res.statusCode).toBe(401);
-                    expect(res.body.code).toBe("UNAUTHORIZED_ACCOUNT");
-                    return;
-                });
+                expect(res.statusCode).toBe(401);
+                expect(res.body.data).toBeUndefined();
+                return;
             });
         });
     });
